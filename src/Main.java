12cd55095e0bcfdd69446bd27ce5fce843b519c6
poriron <pisitpokpoon@gmail.com>
2019-11-27 09:45:03 +0700

    commit-l1

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		//int a, b, c=2;
+		//double gpa=3.50;
+		String FirsName = "Pisit";
+		
+		//for(int i=0 ; i<10 ; i++)
+		//	if( i % 2 == 0)
+		//			System.out.println("Hello " + i + 1);
+		
+		//int[] numbers = {1, 1, 2, 3, 5};
+		//int[] even = new int[20];
+		
+		//String[] fruits = {"banana", "apple", "melon"};
+		//	for(int i=0 ; i<fruits.length ; i++)
+		//		System.out.println(fruits[i]);
+        
+		//for(String f : fruits)
+		//	System.out.println(f);
+		
+		//String
+	    //System.out.println(FirsName.charAt(1));
+	    
+	    String tel = "053-211-122";
+	    String[] sp = tel.split("-");
+	    System.out.println("("+sp[0]+")"+sp[1]);
+	
+	}
+
+}
